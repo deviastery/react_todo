d95de63b14874a5ce389828105af1e9f2add7afb
Viktoria T <deviastery@gmail.com>
2024-09-22 15:28:09 +0300

    gh-pages setting

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { todo } from "../models/todos";
 import delete_icon from "../assets/img/DeleteIcon.svg";
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import TodoItem from "./TodoItem";
 import { todo } from "../models/todos";
 
